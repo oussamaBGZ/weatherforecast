44d66149557b5086db668fa801554a226befea1e
oussama.boughizene <oussama.boughizene@digitarm.com>
2022-05-27 18:52:57 +0200

    add url weather

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { useQuery } from 'react-query'
-import { cityDetails, getCityCoordinates, getWeathersfivedays, weathers } from './api/weatherApi';
+import { cityDetails, getCityCoordinates, getWeathers, weathers } from './api/weatherApi';
 import { useEffect, useState } from 'react';
 
 
@@ -8,24 +8,24 @@ function App() {
   const [submited,setsubmited]=useState(false)
   const [input,setinput]=useState("")
 
-  const { isLoading, error, data: cityDetail } = useQuery<cityDetails, ErrorConstructor>(['getcityDetail',input], () => getCityCoordinates(input),{
-    // The query will not execute until the until we get the response of cirtydetail exists
-    enabled: !!submited,
-    onError:()=>{
-      setsubmited(false)
-    },
-  })
+  // const { isLoading, error, data: cityDetail } = useQuery<cityDetails, ErrorConstructor>(['getcityDetail',input], () => getCityCoordinates(input),{
+  //   // The query will not execute until the until we get the response of cirtydetail exists
+  //   enabled: !!submited,
+  //   onError:()=>{
+  //     setsubmited(false)
+  //   },
+  // })
 
-  const lon = cityDetail?.lon!
-  const lat = cityDetail?.lat!
+  // const lon = cityDetail?.lon!
+  // const lat = cityDetail?.lat!
 
   // Then get the user's projects
-  const { isIdle, data: weatherData } = useQuery<weathers, ErrorConstructor>(
+  const { isLoading, error, data: weatherData } = useQuery<weathers, ErrorConstructor>(
     ['weatherData',input],
-    () => getWeathersfivedays(lat, lon),
+    () => getWeathers(input),
     {
       // The query will not execute until the until we get the response of cirtydetail exists
-      enabled: !!lon && !!lat && !!submited,
+      enabled: !!submited,
       onSuccess:()=>{
         setsubmited(false)
       },
--- src/api/weatherApi.ts
+++ src/api/weatherApi.ts
@@ -15,11 +15,8 @@ export interface weathers {
 }
 
 export const getCityCoordinates = async (city: string): Promise<cityDetails> => {
-    
-
-        const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=256e61ea35f467c2eacca425bd464ed2`)
-        return { 'lat': res.data.coord.lat, 'lon': res.data.coord.lon }
-   
+    const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=256e61ea35f467c2eacca425bd464ed2`)
+    return { 'lat': res.data.coord.lat, 'lon': res.data.coord.lon }
 }
 
-export const getWeathersfivedays = async (lat: number, lon: number): Promise<weathers> => await axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=762385c71cb82e47ad4fdd68f06f6271&units=metric`)
+export const getWeathers = async (city: string): Promise<weathers> => await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=762385c71cb82e47ad4fdd68f06f6271&units=metric`)
